a63d8190116c208e4872903ec7ff913e207e7d35
meinhoongagan <gagansharma3002@gmail.com>
2025-05-29 16:00:14 +0530

    CORS Update

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ connectDB();
 // Middleware
 app.use(express.json({limit:"50mb"}));
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: ["http://localhost:5173", "http://localhost:3000","https://climate-change-app.vercel.app"],
     credentials: true,
 }));
 
